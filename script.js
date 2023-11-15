0047cb3b37bbc634472302f4e24a86d7fc301ddf
Victor Volkov <vityan2504@gmail.com>
2023-11-15 14:54:45 +0300

    Поменял фон и убрал рекламу

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 'use strict';
 
 const books = document.querySelectorAll('.book');
+const adv = document.querySelector('.adv');
+
+const editDoc = function() {
+   document.body.style.backgroundImage = 'url("image/you-dont-know-js.jpg")';
+   adv.remove();
+}
 
 const changeOrder = function() {
    books[0].before(books[1]);
    books[5].after(books[2]);
    books[3].before(books[4]);
-}
-
-
+};
+editDoc();
 changeOrder();
