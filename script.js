ef4fe22170b89b6ccee842da90aecdce6f865805
Victor Volkov <vityan2504@gmail.com>
2023-11-15 14:44:56 +0300

    Восстановил порядок книг

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const books = document.querySelectorAll('.book');
+
+const changeOrder = function() {
+   books[0].before(books[1]);
+   books[5].after(books[2]);
+   books[3].before(books[4]);
+}
+
+
+changeOrder();
